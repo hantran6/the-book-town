1f0993bd550e084f3346b37d0bb1ce0bea1bab43
hantran6 <s3963227@rmit.edu.vn>
2024-01-12 23:11:28 +0700

    change root to ec2-user

--- db/sql/set-root-password.sql
+++ db/sql/set-root-password.sql
@@ -2,7 +2,7 @@
 -- Creates a root user that can connect from any host and sets the password for all root users in Mariadb
 --
 USE mysql;
-CREATE user 'root'@'%';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-UPDATE user SET password=PASSWORD("123456") WHERE user='root';
+CREATE user 'ec2-user'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'ec2-user'@'%';
+UPDATE user SET password=PASSWORD("123456") WHERE user='ec2-user';
 flush privileges;
